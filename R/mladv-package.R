3cdcf34ebecce0090e244d11afe874532d980c9f
thanhuwe8 <thanhuwe8@gmail.com>
2019-01-18 22:26:56 +0100

    badge

--- R/mladv-package.R
+++ R/mladv-package.R
@@ -1,5 +1,16 @@
+#' Package mladv
+#'
+#' The goal of the package is to provide tools to clean, convert and analyze
+#' financial data
+#' @section Help:
+#' For a quick help see the README file.
+#' For more details see the vignette
+#'
+#' @docType package
+#' @name mladv-package
 #' @keywords internal
 #' @useDynLib mladv, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 NULL
+#'
 "_PACKAGE"
